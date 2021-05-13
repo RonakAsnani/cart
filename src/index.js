52ef8347e1578abc0695b691502c3353834f52a3
Ronak <ronakasnani5@gmail.com>
2021-05-13 17:10:33 +0530

    created new master

--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-import firebase from 'firebase';
-import 'firebase/firestore';
+import firebase from "firebase";
+import "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB_jQGC3O_6NOhQYHPoou_1OTe6cCNDjII",
-  authDomain: "cart-a4fae.firebaseapp.com",
-  projectId: "cart-a4fae",
-  storageBucket: "cart-a4fae.appspot.com",
-  messagingSenderId: "703369410052",
-  appId: "1:703369410052:web:5b511a37639fbd3f536751"
-};
+const firebaseConfig = {};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
